02f9b6e0f9a973e5225e871d5742749a5b7ddbba
srikanth chary <srikathchary1998@gmail.com>
2022-06-04 09:23:19 +0530

    hdghd

--- run_wrapper.bat
+++ run_wrapper.bat
@@ -4,5 +4,5 @@ venv/scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-pythoncommand = ("bat execute_wrapper.py)
+bat execute_wrapper.py
 echo "calling: ${pythoncommand}"
